76acac34468bae6c07a84b4214ee9e70b8e53ff1
MaximPirozhenko <fatal1ty.nur@mail.ru>
2016-11-26 01:54:40 +0400

    Update calc_controller.rb

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -7,7 +7,7 @@ class CalcController < ApplicationController
     result = processing(params[:v2])
     @kolvo_posl = result.size
     return unless @kolvo_posl.nonzero?
-      @inputed = params[:v2].split(' ').join(', ')
+      @inputed = params[:v2].split(' ')
       @max_posl = result.max_by(&:size).join(', ')
       @all = result.map(&:to_s).join(', ')
     end
