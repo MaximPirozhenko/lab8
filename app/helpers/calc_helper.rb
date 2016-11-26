33c7d73f8412fceb3c711e4c960530b379c5b153
MaximPirozhenko <fatal1ty.nur@mail.ru>
2016-11-27 01:36:53 +0400

    Update calc_helper.rb

--- app/helpers/calc_helper.rb
+++ app/helpers/calc_helper.rb
@@ -1,18 +1,12 @@
 module CalcHelper
-  def processing(v)
-    result = []
-    substring = []
-    array = v.split(' ').map(&:to_i)
-    array.each_with_index do |x,ind|
-      if (0..Float::INFINITY).lazy.take_while{|y| 5**y <= x}.find{|y| x == 5**y}
-        substring << x
-        result << substring if ind == array.size - 1
-      elsif  substring.any?
-        result << substring
-        substring = []
-      end
-    end
-    result
-  end
 
+  def processing(v)
+    v.split(' ').map(&:to_i)
+        .split{|el| (0..Float::INFINITY)
+                        .lazy
+                        .take_while { |y| 5**y <= el }
+                        .all?{ |y| 5**y != el }
+    }
+        .reject(&:empty?)
+  end
 end
