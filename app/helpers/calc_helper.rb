bbadf8eefa7e97f2eb90ec2d67fe16605ad7f21b
MaximPirozhenko <fatal1ty.nur@mail.ru>
2016-11-27 02:36:07 +0400

    Update calc_helper.rb

--- app/helpers/calc_helper.rb
+++ app/helpers/calc_helper.rb
@@ -2,7 +2,7 @@ module CalcHelper
 
   def processing(v)
     v.split(' ').map(&:to_i)
-        .split{|el| Math::log(el,5).to_s.split('.')[1].to_i.nonzero?}
+        .split{|el| Math::log(el,5).modulo(1).nonzero?}
         .reject(&:empty?)
   end
 
